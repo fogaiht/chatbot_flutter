8f90c9c95cee72e4683ee2ffb52705f521a42d0a
Thiago Fernandes <thiagofpf@gmail.com>
2020-09-15 09:51:01 -0300

    work in progress

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dialogflow/dialogflow_v2.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+
+import '../../shared/models/chat_message.dart';
 import 'home_controller.dart';
+import 'widgets/chat_message_list_item.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -13,14 +17,110 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends ModularState<HomePage, HomeController> {
   //use 'controller' variable to access controller
 
+  final _messageList = <ChatMessage>[];
+  final _controllerText = TextEditingController();
+
+  @override
+  void dispose() {
+    _controllerText.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Chatbot - Professor"),
       ),
       body: Column(
-        children: <Widget>[],
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => ChatMessageListItem(chatMessage: _messageList[index]),
+              itemCount: _messageList.length,
+            ),
+          ),
+          Divider(height: 1.0),
+          _buildUserInput(),
+        ],
+      ),
+    );
+  }
+
+  void _sendMessage({String text}) {
+    _controllerText.clear();
+    _addMessage(name: 'Thiago Fernandes', text: text, type: ChatMessageType.sent);
+  }
+
+  // Adiciona uma mensagem na lista de mensagens
+  void _addMessage({String name, String text, ChatMessageType type}) {
+    var message = ChatMessage(text: text, name: name, type: type);
+    setState(() {
+      _messageList.insert(0, message);
+    });
+
+    if (type == ChatMessageType.sent) {
+      // Envia a mensagem para o chatbot e aguarda sua resposta
+      _dialogFlowRequest(query: message.text);
+    }
+  }
+
+  Future _dialogFlowRequest({String query}) async {
+    // Adiciona uma mensagem temporária na lista
+    _addMessage(name: 'Professor', text: 'Escrevendo...', type: ChatMessageType.received);
+
+    // Faz a autenticação com o serviço, envia a mensagem e recebe uma resposta da Intent
+    AuthGoogle authGoogle = await AuthGoogle(fileJson: "assets/credentials.json").build();
+    Dialogflow dialogflow = Dialogflow(authGoogle: authGoogle, language: "pt-BR");
+    AIResponse response = await dialogflow.detectIntent(query);
+
+    // remove a mensagem temporária
+    setState(() {
+      _messageList.removeAt(0);
+    });
+
+    // adiciona a mensagem com a resposta do DialogFlow
+    _addMessage(name: 'Professor', text: response.getMessage() ?? '', type: ChatMessageType.received);
+  }
+
+  Widget _buildTextField() {
+    return Flexible(
+      child: TextField(
+        controller: _controllerText,
+        decoration: InputDecoration.collapsed(
+          hintText: "Enviar mensagem",
+        ),
+      ),
+    );
+  }
+
+  // Botão para enviar a mensagem
+  Widget _buildSendButton() {
+    return new Container(
+      margin: new EdgeInsets.only(left: 8.0),
+      child: new IconButton(
+          icon: new Icon(Icons.send, color: Theme.of(context).accentColor),
+          onPressed: () {
+            if (_controllerText.text.isNotEmpty) {
+              _sendMessage(text: _controllerText.text);
+            }
+          }),
+    );
+  }
+
+  // Monta uma linha com o campo de text e o botão de enviao
+  Widget _buildUserInput() {
+    return Container(
+      color: Colors.white,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: new Row(
+        children: <Widget>[
+          _buildTextField(),
+          // Text("ou"),
+          _buildSendButton(),
+        ],
       ),
     );
   }
